efb40e2df853e4ddf74575b01fe644eff971b8ff
eduardor2m <deveduardomelo@gmail.com>
2023-03-15 01:40:18 -0300

    feat: update

--- application/controllers/user_test.go
+++ application/controllers/user_test.go
@@ -1,40 +1,51 @@
 package controllers_test
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/eduardor2m/echo-go/application/controllers"
 	"github.com/eduardor2m/echo-go/domain"
+	"github.com/eduardor2m/echo-go/framework/utils"
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
 )
 
-func SetupRouter() *echo.Echo {
-	e := echo.New()
-	e.POST("/users", controllers.CreateUser)
-	e.GET("/users/:id", controllers.GetUser)
-	e.GET("/users", controllers.GetUsers)
-	return e
-}
-
 func TestCreateUser(t *testing.T) {
-	e := SetupRouter()
-	user := domain.User{
-		Name:     "Eduardo",
-		Email:    "eduardo@gmail.com",
-		Password: "123456",
+	e := echo.New()
+
+	reqBody := map[string]string{
+		"name":     "Eduardo",
+		"email":    "eduardo@gmail.com",
+		"password": "123456",
 	}
-	req := httptest.NewRequest(http.MethodPost, "/users", nil)
+
+	defer func() {
+		db := utils.ConnectDb()
+		db.Exec("DROP DATABASE users")
+	}()
+
+	reqJSON, _ := json.Marshal(reqBody)
+
+	req := httptest.NewRequest(http.MethodPost, "/users", bytes.NewBuffer(reqJSON))
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	rec := httptest.NewRecorder()
+
 	c := e.NewContext(req, rec)
-	c.SetPath("/users")
-	c.SetParamNames("name", "email", "password")
-	c.SetParamValues(user.Name, user.Email, user.Password)
+
 	if assert.NoError(t, controllers.CreateUser(c)) {
 		assert.Equal(t, http.StatusCreated, rec.Code)
+
+		var user domain.User
+
+		if assert.NoError(t, json.Unmarshal(rec.Body.Bytes(), &user)) {
+			assert.Equal(t, "Eduardo", user.Name)
+			assert.Equal(t, "eduardo@gmail.com", user.Email)
+		}
+
 	}
 
 }
